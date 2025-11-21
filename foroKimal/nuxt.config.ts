5a5102a63e7ef0b586494e3a7b7fc6f824c5301b
BayonSD <bayonsd.dev@gmail.com>
2025-11-20 21:02:01 -0300

    posible fix?

--- foroKimal/nuxt.config.ts
+++ foroKimal/nuxt.config.ts
@@ -1,16 +1,22 @@
 import path from "path";
 
 const alias = {
-	"@sass": path.resolve(__dirname, "./sass"),
+    "@sass": path.resolve(__dirname, "./sass"),
 };
 
 export default defineNuxtConfig({
-	compatibilityDate: '2025-07-15',
-	devtools: { enabled: true },
-	alias,
+    compatibilityDate: '2025-07-15',
+    devtools: { enabled: true },
+    alias,
 
-	css: [
-		path.resolve(__dirname, "./sass/base.sass"),
-		path.resolve(__dirname, "./sass/theme.sass")
-	]
+    css: [
+        path.resolve(__dirname, "./sass/base.sass"),
+        path.resolve(__dirname, "./sass/theme.sass")
+    ],
+
+    runtimeConfig: {
+        public: {
+            apiUrl: process.env.NUXT_PUBLIC_API_URL || 'http://localhost:5000'
+        }
+    }
 })
